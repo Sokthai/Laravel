a2ee99ec2bf92a640fc65fe8f39955d78bd4a0e9
System Administrator <root@Allens-MacBook-Pro-2.local>
2017-01-15 12:52:48 -0500

    set up gulp and bootstrap

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laracast Project Flyer 2</title>
+    <link rel="stylesheet" href="/css/app.css">
+
+
+
+</head>
+<body>
+<div classs="container">
+    @yield('content')
+</div>
+</body>
+</html>
